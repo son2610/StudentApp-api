3fe3cde234d4f90cf0bfd6b4d23226bdf5346b67
Son-Blue <okmen586999@gmail.com>
2024-01-19 15:17:51 +0700

    debug

--- api/server.js
+++ api/server.js
@@ -1,6 +1,9 @@
+// See https://github.com/typicode/json-server#module
 const jsonServer = require("json-server");
+
 const server = jsonServer.create();
 
+// Uncomment to allow write operations
 const fs = require("fs");
 const path = require("path");
 const filePath = path.join("db.json");
@@ -8,10 +11,13 @@ const data = fs.readFileSync(filePath, "utf-8");
 const db = JSON.parse(data);
 const router = jsonServer.router(db);
 
+// Comment out to allow write operations
+// const router = jsonServer.router('db.json')
+
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-
+// Add this before server.use(router)
 server.use(
     jsonServer.rewriter({
         "/api/*": "/$1",
@@ -19,8 +25,9 @@ server.use(
     })
 );
 server.use(router);
-server.listen(8080, () => {
+server.listen(3000, () => {
     console.log("JSON Server is running");
 });
 
+// Export the Server API
 module.exports = server;
